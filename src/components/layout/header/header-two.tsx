5a3c9c5fe5dc2c3394e962f7e53e9525d324a4c3
opppdia <hannan.opppdia@gmail.com>
2022-09-20 13:15:02 +0600

    Zip design cheanges done

--- src/components/layout/header/header-two.tsx
+++ src/components/layout/header/header-two.tsx
@@ -152,9 +152,10 @@ const Header: React.FC = () => {
                       <div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
                         <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                           <div className="sm:flex sm:items-start">
-                            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                            <div className="locationtxtxtx  ">
                               {/* <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" /> */}
-                              <p>Icons</p>
+                              <p>Locations</p>
+                              <MdLocationOn className="lcoationmtmt" />
                             </div>
                             <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                               <Dialog.Title
@@ -185,7 +186,7 @@ const Header: React.FC = () => {
                               {zipCodeResultare && (
                                 <Dialog.Title
                                   as="h3"
-                                  className="text-lg leading-6 font-medium text-gray-900"
+                                  className="delirvrtyTxt text-lg leading-6 font-medium text-gray-900"
                                 >
                                   We are delivery at: {zipCodeResultare}
                                 </Dialog.Title>
@@ -196,11 +197,11 @@ const Header: React.FC = () => {
                         <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                           <button
                             type="button"
-                            className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-yellow-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                            className="btnZipColor mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                             // onClick={() => setOpen(false)}
                             onClick={() => areasearch()}
                           >
-                            FInd
+                            search
                           </button>
                           <button
                             type="button"
